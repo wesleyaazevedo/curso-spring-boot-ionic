bf265d05ed8a4c706777c5826fa9f04bc88ba30a
wesleyaazevedo <wesleyaazevedo@gmail.com>
2018-05-27 01:19:04 -0400

    Criado Endpoints /pedidos/{id}

--- src/main/java/com/cursomc/uml/domain/Pagamento.java
+++ src/main/java/com/cursomc/uml/domain/Pagamento.java
@@ -11,6 +11,7 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 import com.cursomc.uml.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 @Inheritance(strategy=InheritanceType.JOINED)
@@ -23,6 +24,7 @@ public abstract class Pagamento implements Serializable{
 	private Integer estado;
 	
 	//Associacoes
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
--- src/main/java/com/cursomc/uml/domain/Pedido.java
+++ src/main/java/com/cursomc/uml/domain/Pedido.java
@@ -17,6 +17,8 @@ import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Pedido implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -29,9 +31,11 @@ public class Pedido implements Serializable{
 	private Date instante;
 	
 	//Associacoes
+	@JsonManagedReference
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
 	
+	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
